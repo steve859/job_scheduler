48be23b750297d432eccfbdb968313bdf76973a2
PhamPhuc0903 <Pphuc8554@gmail.com>
2025-12-11 14:07:59 +0700

    feat(orders): add dto request and enum OrderStatus

---
+++ orders/src/main/java/com/example/orders/dto/request/OrderUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.example.orders.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderUpdateRequest {
+    @NotNull
+    private List<OrderItemRequest> items;
+}
